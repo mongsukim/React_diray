bad9e2ca07bee72b2c83c0fca39e9e88cd2df934
hskim0408 <hskim0408@medicalip.com>
2022-02-27 17:18:36 +0900

    header 월 세팅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from "react";
+import React, { useReducer, useRef } from 'react';
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -22,62 +22,99 @@ const reducer = (state, action) => {
       break;
     }
     case 'EDIT': {
-      newState = state.map((it) => it.id === action.data.id ? { ...action.data } : it)
-      break
-      }
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
     default:
       return state;
   }
   return newState;
-}
+};
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
-function App() {
 
-  const [data, dispatch] = useReducer(reducer, []);
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: '오늘의일기 1번',
+    date: 1645947073826,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: '오늘의일기 2번',
+    date: 1645947073827,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: '오늘의일기 3번',
+    date: 1645947073828,
+  },
+
+  {
+    id: 4,
+    emotion: 4,
+    content: '오늘의일기 4번',
+    date: 1645947073829,
+  },
+
+  {
+    id: 5,
+    emotion: 5,
+    content: '오늘의일기 5번',
+    date: 1645947073830,
+  },
+];
+console.log(new Date().getTime());
+function App() {
+  const [data, dispatch] = useReducer(reducer, dummyData);
   const dataId = useRef(0);
   //CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({
-      type: "CREATE",
+      type: 'CREATE',
       data: {
         id: dataId.current,
         date: new Date(date).getTime(),
         content,
         emotion,
-      }
+      },
     });
     dataId.current += 1;
   };
   //REMOVE
   const onRemove = (targetId) => {
-    dispatch({type:"REMOVE", targetId})
-  }
+    dispatch({ type: 'REMOVE', targetId });
+  };
   //EDIT
   const onEdit = (targetId, date, content, emotion) => {
     dispatch({
-      type: "EDIT",
+      type: 'EDIT',
       date: {
         id: targetId,
         date: new Date(date).getTime(),
         content,
-        emotion
-      }
-    })
-  }
+        emotion,
+      },
+    });
+  };
   return (
     <DiaryStateContext.Provider value={data}>
-      <DiaryDispatchContext.Provider value={{onCreate,onEdit,onRemove}}>
-      <BrowserRouter>
-        <div className='App'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/new' element={<New />} />
-            <Route path='/edit' element={<Edit />} />
-            <Route path='/diary/:id' element={<Diary />} />
-          </Routes>
-        </div>
+      <DiaryDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className='App'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/new' element={<New />} />
+              <Route path='/edit' element={<Edit />} />
+              <Route path='/diary/:id' element={<Diary />} />
+            </Routes>
+          </div>
         </BrowserRouter>
       </DiaryDispatchContext.Provider>
     </DiaryStateContext.Provider>
