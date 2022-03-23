f56c590fbba47f0b1daf44de7cb80ba39f772c8f
hskim0408 <hskim0408@medicalip.com>
2022-03-23 16:11:05 +0900

    감정표기 작업중

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -3,16 +3,49 @@ import { useNavigate } from 'react-router-dom';
 
 import MyHeader from './MyHeader';
 import MyButton from './MyButton';
+import EmotionItem from './EmotionItem';
+
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript:'완전 좋음'
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript:'좋음'
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript:'그럭저럭'
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript:'나쁨'
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript:'끔찍함'
+  }
+]
 
 const getStringDate = (date) => {
   return date.toISOString().slice(0,10)
 }
 
 const DiaryEditor = () => {
+    const [emotion, setEmotion] = useState(3)
     const [date,setDate] = useState(getStringDate(new Date()))
     const navigate = useNavigate();
   return (
-    <div>
+    <div className="DiaryEditor">
       <MyHeader
         headText={"새 일기쓰기"}
         leftChild={<MyButton text={'<뒤로가기'} />}
@@ -21,12 +54,20 @@ const DiaryEditor = () => {
       <div>
         <section>
           <h4>오늘은 몇일인가요?</h4>
-          <div className="input-box">
+          <div className="input_box">
             <input
-              className="input-date"
+              className="input_date"
               value={date}
               onChange={(e) => setDate(e.target.value)}
               type="date" />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem key={it.emotion_id}{...it}/>
+            ))}
           </div>
         </section>
       </div>
